0a13280b933d51db87d65ca24d476f99f52c3799
Shivam Parikh <shivamparikh@Anjanis-MacBook-Pro.local>
2026-03-23 10:58:24 +0530

    Revert to original Landing at /

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Landing from './pages/Landing'
-import Landing2 from './pages/Landing2'
 import Prototype from './pages/Prototype'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing2 />} />
-        <Route path="/v1" element={<Landing />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/prototype" element={<Prototype />} />
       </Routes>
     </BrowserRouter>
